50ae1393c82b0768827b7ca8c9e4956e6e6e5df0
astro11 <aquila.strother@asu.edu>
2018-06-20 17:59:23 -0400

    added graph edges/vertex, and fixed Vertex pos arg that was causing render fail

--- src/draw.py
+++ src/draw.py
@@ -11,11 +11,15 @@ graph_data = Graph()
 graph_data.debug_create_test_data()
 print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-debug_palette = Spectral8
-debug_palette += ('#ff0000', '#0080ff') #debug_palette.append('#ff0000')
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
+
+# debug_palette = Spectral8
+# debug_palette += ('#ff0000', '#0080ff') #debug_palette.append('#ff0000')
 
 
 plot = figure(title='PythonBokeh Graph Demonstration', x_range=(0, 500), y_range=(0, 500),
@@ -24,18 +28,22 @@ plot = figure(title='PythonBokeh Graph Demonstration', x_range=(0, 500), y_range
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_palette, 'color')
+graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
+# This is drawing the edges from start to end
+# TODO: Change how this works
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=[0]*N, # This is a list that has something to do with starting points | Why does it work?
+    end=node_indices) # This is a list that has something to do with ending points | Why does it work?
 
 ### start of layout code
-# setting the positions of vertexes
-circ = [i*2*math.pi/N for i in node_indices] #divided circumference by node count
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+# this is setting the positions of the vertexes
+# circ = [i*2*math.pi/N for i in node_indices] #divided circumference by node count
+
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
--- src/graph.py
+++ src/graph.py
@@ -5,8 +5,8 @@ class Edge:
 class Vertex:
     def __init__(self, value, **pos): # TODO: Test default value arguments
         self.value = value
-        self.color = 'white'
-        self.position = pos
+        self.color = 'blue'
+        self.pos = pos
         self.edges = []
 
 class Graph:
@@ -14,10 +14,18 @@ class Graph:
         self.vertexes = []
 
     def debug_create_test_data(self):
-        debug_vertex_1 = Vertex('T1', x=40, y=40)
-        debug_vertex_2 = Vertex('T2', x=140, y=140)
+        debug_vertex_1 = Vertex('T1', x = 40, y = 40)
+        debug_vertex_2 = Vertex('T2', x = 140, y = 140)
+        debug_vertex_3 = Vertex('T3', x = 300, y = 400)
+        debug_vertex_4 = Vertex('T4', x = 380, y = 480)
 
-        debug_edge_1 = Edge(debug_vertex_2)
+        debug_edge_1 = Edge(debug_vertex_2)        
         debug_vertex_1.edges.append(debug_edge_1)
-    
-        self.vertexes.extend([debug_vertex_1, debug_vertex_2])
+
+        debug_edge_2 = Edge(debug_vertex_2)
+        debug_vertex_3.edges.append(debug_edge_2)
+
+        debug_edge_3 = Edge(debug_vertex_3)
+        debug_vertex_4.edges.append(debug_edge_3)
+
+        self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4])
